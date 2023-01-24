9055632b13849ee9699d35c0b849d70e002e8a65
TarunGambhi <118712515+TarunGambhi@users.noreply.github.com>
2023-01-24 11:32:15 +0530

    Update HIndex.java

--- HIndex.java
+++ HIndex.java
@@ -14,3 +14,21 @@ class Solution {
         return 0;
     }
 }
+
+//TC:o(log n)
+//sc:o(1)
+class Solution {
+    public int hIndex(int[] citations) {
+        int low = 0;
+        int high = citations.length-1;
+        int len = citations.length;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int rPapers = len - mid;
+            if(citations[mid] == rPapers) return rPapers;
+            else if(citations[mid] > rPapers) high = mid - 1;
+            else low = mid + 1;
+        }
+        return len - low;
+    }
+}
